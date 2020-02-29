ee538ec3458779fb42488b3ce8e3b4568f5bb45a
Justin Marchi <marchij86@gmail.com>
2020-02-28 23:19:27 -0500

    Pushing the completed html, javascript, and css code for the javascript random password generator

---
+++ pseudo-code.txt
@@ -0,0 +1,13 @@
+-When the generate password button is pressed, a series of prompts needs to appear.
+-The prompt needs to ask which criteria the user wants to include:
+-(length of password, and whether to include uppercase, lowercase, and/or special characters)
+-The uppercase, lowercase, and special characters should be accessed in an array
+-Each prompt needs to store the input in a variable
+-There needs to be a validation to prevent the user from not selecting any criteria
+-At the end of the prompts, a random number generator needs to run over each array and output a password the length selected.
+-The password then needs to be returned and output in the password form field
+
+-Will need a varaible for: password length, lowercase characters, uppercase characters, special characters, 
+-Will need a random number generator to iterate the number of times stored in the password length variable
+-Function to generate the password is called generatePassword();
+-The generatePassword() function will return the password to the password variable
---
+++ script.js
@@ -0,0 +1,161 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
+
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
+
+    if (passwordLength<8){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'N' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
+}
+
+//Function used to determine whether the user wants to include uppercase characters in the password
+function determineUppercase(){
+  uppercaseCheck = prompt("Do you want to include uppercase letters in your password? (Y/N)");
+    uppercaseCheck = uppercaseCheck.toLowerCase();
+
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Please answer Y or N");
+      determineUppercase();
+
+    }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
+      uppercaseCheck = true;
+      return uppercaseCheck;
+
+    }else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
+      uppercaseCheck = false;
+      return uppercaseCheck;
+    
+    }else {
+      alert("Please answer Y or N");
+      determineUppercase();
+    }
+    return uppercaseCheck;
+}
+
+//Function used to determine whether the user wants to include numbers in the password
+function determineNumbers(){
+  numberCheck = prompt("Do you want to include numbers in your password? (Y/N)");
+    numberCheck = numberCheck.toLowerCase();
+
+    if (numberCheck === null || numberCheck === ""){
+      alert("Please answer Y or N");
+      determineNumbers();
+
+    }else if (numberCheck === "yes" || numberCheck ==="y"){
+      numberCheck = true;
+      return numberCheck;
+
+    }else if (numberCheck === "no" || numberCheck ==="n"){
+      numberCheck = false;
+      return numberCheck;
+    
+    }else {
+      alert("Please answer Y or N");
+      determineNumbers();
+    }
+    return numberCheck;
+}
+
+//Function used to determine whether the user wants to include special characters in the password
+function determineSpecial(){
+  specialCheck = prompt("Do you want to include special characters in your password? (Y/N)");
+    specialCheck = specialCheck.toLowerCase();
+
+    if (specialCheck === null || specialCheck === ""){
+      alert("Please answer Y or N");
+      determineSpecial();
+
+    }else if (specialCheck === "yes" || specialCheck ==="y"){
+      specialCheck = true;
+      return specialCheck;
+
+    }else if (specialCheck === "no" || specialCheck ==="n"){
+      specialCheck = false;
+      return specialCheck;
+    
+    }else {
+      alert("Please answer Y or N");
+      determineSpecial();
+    }
+    return specialCheck;
+}
+
+ //Function used to take all the input from the previous functions and generate a password using a random number generator and 
+ //the charAt method 
+function generatePassword(){
+  determineLength();
+  console.log(passwordLength);
+  determineUppercase();
+  console.log(uppercaseCheck);
+  determineNumbers();
+  console.log(numberCheck);
+  determineSpecial();
+  console.log(specialCheck);
+
+var characters = lowercaseChar;
+var password = "";
+if (uppercaseCheck && numberCheck && specialCheck){
+  characters += uppercaseChar + numberChar + specialChar;
+
+}else if (uppercaseCheck && numberCheck){
+  characters += uppercaseChar + numberChar;
+
+}else if (numberCheck && specialCheck){
+  characters += numberChar + specialChar;
+
+}else if (uppercaseCheck && specialCheck){
+  characters += uppercaseChar + specialChar;
+
+}else if (uppercaseCheck){
+  characters += uppercaseChar;
+
+}else if(numberCheck){
+  characters += numberChar;
+
+}else if (specialCheck){
+  characters += specialChar;
+
+}else{
+  characters === lowercaseChar;
+}
+
+  for(var i = 0; i < passwordLength; i++){
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password1 = "";
+  password1 = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password1;
+}
+
+function resetText(){
+  document.getElementById("password").value = "Your Secure Password";
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
